cf99a3d49122e79f27dc4b632546d95b99fb4074
hrjha97 <jha.harsh837@gmail.com>
2024-08-11 17:14:38 +0530

    backend setup done

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: "postgresql://a-mock-interview_owner:M5gDVFH3cBPl@ep-wispy-lab-a1alho1g.ap-southeast-1.aws.neon.tech/a-mock-interview?sslmode=require",
+    }
+  };
