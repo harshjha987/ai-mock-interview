164a8818a1522131a9807c7432c125d63627b8e7
hrjha97 <jha.harsh837@gmail.com>
2024-08-14 00:16:02 +0530

    ai model integrated

--- app/dashboard/_components/AddNewInterview.js
+++ app/dashboard/_components/AddNewInterview.js
@@ -16,6 +16,10 @@ import { chatSession } from "../../../utils/GeminiAiModel";
 import { LoaderCircle } from "lucide-react";
 import { db } from "../../../utils/db";
 import { MockInterview } from "../../../utils/schema";
+import { uuid } from "drizzle-orm/pg-core";
+import { v4 as uuidv4 } from 'uuid';
+import { useUser } from "@clerk/nextjs";
+import moment from "moment/moment";
   
 
 export default function AddNewInterview(){
@@ -25,6 +29,7 @@ export default function AddNewInterview(){
     const[jobExperience,setJobExperience] = useState();
     const[loading,setLoading] = useState(false)
     const[jsonResponse , setJsonResponse] = useState([]);
+    const {user} = useUser();
     const onSubmit = async(event)=>{
         setLoading(true);
         event.preventDefault();
@@ -32,11 +37,27 @@ export default function AddNewInterview(){
         const InputPrompt = "Job Position :"+jobPosition+" , Job Description : "+jobDescription+", Years of Experience : "+process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT+" ,Depending on Job Position , Job Description and Years of Experience give us 5 interview question along with answers in Json format,Give us question and answer field on json"
             const result = await chatSession.sendMessage(InputPrompt);
             const MockJsonResp = (result.response.text()).replace('```json',"").replace('```',"")
-            
+            console.log(JSON.parse(MockJsonResp));
             setJsonResponse(MockJsonResp);
+            if(MockJsonResp){
+
+            
             const response  = await db.insert(MockInterview).values({
-                
-            })
+                mockId : uuidv4(),
+                jsonMockResp : MockJsonResp,
+                jobPosition: jobPosition,
+                jobDescription : jobDescription,
+                jobExperience : jobExperience,
+                createdBy :user?.primaryEmailAddress?.emailAddress,
+                createdAt : moment().format('DD-MM-yyyy')
+            }).returning({mockId : MockInterview.mockId})
+            console.log("Id : ", response)
+            if(response){
+                setOpenDialog(false)
+            }
+        }else{
+            console.log("Error");
+        }
             setLoading(false)
     }
     return(
