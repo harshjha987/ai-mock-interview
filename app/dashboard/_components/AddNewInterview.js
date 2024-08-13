052ad65cff8e6cdb8f707d47ec15d7cab2091dad
hrjha97 <jha.harsh837@gmail.com>
2024-08-13 23:55:45 +0530

    added gemini api for mock qustns

--- app/dashboard/_components/AddNewInterview.js
+++ app/dashboard/_components/AddNewInterview.js
@@ -12,10 +12,33 @@ import {
   } from "../../../components/ui/dialog"
 import { Input } from "../../../components/ui/input"
 import { Textarea } from "../../../components/ui/textarea";
+import { chatSession } from "../../../utils/GeminiAiModel";
+import { LoaderCircle } from "lucide-react";
+import { db } from "../../../utils/db";
+import { MockInterview } from "../../../utils/schema";
   
 
 export default function AddNewInterview(){
     const [openDialog,setOpenDialog] = useState(false);
+    const[jobPosition,setJobPosition] = useState();
+    const[jobDescription,setJobDescription] = useState();
+    const[jobExperience,setJobExperience] = useState();
+    const[loading,setLoading] = useState(false)
+    const[jsonResponse , setJsonResponse] = useState([]);
+    const onSubmit = async(event)=>{
+        setLoading(true);
+        event.preventDefault();
+        console.log(jobPosition,jobDescription,jobExperience)
+        const InputPrompt = "Job Position :"+jobPosition+" , Job Description : "+jobDescription+", Years of Experience : "+process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT+" ,Depending on Job Position , Job Description and Years of Experience give us 5 interview question along with answers in Json format,Give us question and answer field on json"
+            const result = await chatSession.sendMessage(InputPrompt);
+            const MockJsonResp = (result.response.text()).replace('```json',"").replace('```',"")
+            
+            setJsonResponse(MockJsonResp);
+            const response  = await db.insert(MockInterview).values({
+                
+            })
+            setLoading(false)
+    }
     return(
         <div>
             <div  className="border p-10 rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer
@@ -28,27 +51,35 @@ export default function AddNewInterview(){
                 <DialogHeader>
                 <DialogTitle className = "text-2xl font-bold">Tell us more about Job your are interviewing.</DialogTitle>
                 <DialogDescription>
-                <form>
+                <form onSubmit={onSubmit}>
 
                     <div>
                        
                         <h2>Add Details about job position,you skills and year of experience. </h2>
                         <div className="mt-7 my-3">
                             <label>Job Role/Job Position</label>
-                        <Input placeholder = "Ex. Full Stack developer" required></Input>
+                        <Input placeholder = "Ex. Full Stack developer" required
+                        onChange = {(event)=>setJobPosition(event.target.value)}
+                        />
                         </div>
                         <div className="mt-7 my-3">
                             <label>Job Description/Tech Stack (In Short)</label>
-                        <Textarea placeholder = "Ex. React, Angular NodeJS, MySQl etc"  required />
+                        <Textarea placeholder = "Ex. React, Angular NodeJS, MySQl etc"  required 
+                            onChange = {(event)=>setJobDescription(event.target.value)}
+                        />
                         </div>
                         <div className=" my-3">
                             <label>Years of Experience</label>
-                        <Input placeholder = "Ex. 5 years" type = 'number' max = "50"  required></Input>
+                        <Input placeholder = "Ex. 5 years" type = 'number' max = "50"  required
+                        onChange = {(event)=> setJobExperience(event.target.value)}
+                        ></Input>
                         </div>
                     </div>
                     <div className="flex gap-5 justify-end">
                         <Button type = "button" variant = "ghost" onClick= {()=> setOpenDialog(false)}>Cancel</Button>
-                        <Button type = "submit" >Start Interview</Button>
+                        <Button type = "submit" disabled = {loading} >
+                        {loading? <><LoaderCircle className="animate-spin"/>'Generating from Ai' </>: "Start Interview"}
+                        </Button>
                     </div>
                     </form>
                 </DialogDescription>
